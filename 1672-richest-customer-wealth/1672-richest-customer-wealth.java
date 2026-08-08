d0b4104b91033f7c979325dd693b0853ad22f3b9
Ashish Goswami <ashishgoswami885850@gmail.com>
2026-08-08 18:07:03 +0530

    Time: 0 ms (100%), Space: 44.6 MB (37.91%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumWealth(int[][] arr) {
+        int maxsum = 0;
+        for(int i =0;i<arr.length;i++){
+            int sum = 0;
+            for(int j =0;j<arr[0].length;j++){
+                sum +=arr[i][j];
+            }
+            if(sum>maxsum){
+                maxsum = sum;
+            }
+        }
+        return maxsum;
+    }
+}
